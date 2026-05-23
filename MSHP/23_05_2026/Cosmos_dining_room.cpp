7828675338118e894fe7974564f109ba50173ec1
TheBloodyReaper <konst.popov12@gmail.com>
2026-05-23 10:26:11 +0300

    Решил задачу Космостоловая

---
+++ MSHP/23_05_2026/Cosmos_dining_room.cpp
@@ -0,0 +1,24 @@
+//
+//_______________________________________________________________________________________________________________________________
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n, a;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        if (a == 0)
+        {
+            cout << i << " ";
+        }
+    }
+    
+}
